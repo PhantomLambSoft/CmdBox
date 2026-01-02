481ee7babd4cc0a02a13a4ff5d7e5dc92b18dabf
Kyle <kyle.hickey222@gmail.com>
2026-01-02 09:58:38 -0600

    Give default colors better names

--- src/cmdbox/settings/models.py
+++ src/cmdbox/settings/models.py
@@ -3,20 +3,20 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class DefaultColors:
-    name: str = "#a54242"
-    template: str = "#8c9440"
-    description: str = "#de935f"
-    date_created: str = "#5f819d"
-    used: str = "#85678f"
+    red: str = "#a54242"
+    green: str = "#8c9440"
+    orange: str = "#de935f"
+    blue: str = "#5f819d"
+    purple: str = "#85678f"
     teal: str = "#5e8d87"
     light_grey: str = "#707880"
     dark_grey: str = "#373b41"
     pink: str = "#cc6666"
     yellow_green: str = "#b5bd68"
     yellow: str = "#f0c674"
-    updated: str = "#81a2be"
-    last_used: str = "#b294bb"
-    light_blue: str = "#8abeb7"
+    light_blue: str = "#81a2be"
+    light_purple: str = "#b294bb"
+    cyan: str = "#8abeb7"
     white: str = "#c5c8c6"
 
 
@@ -32,29 +32,29 @@ class UiColors:
 
 @dataclass(frozen=True)
 class UiCommandColors:
-    alias: str = DefaultColors.name
-    template: str = DefaultColors.template
-    description: str = DefaultColors.description
-    date_created: str = DefaultColors.date_created
-    last_updated: str = DefaultColors.updated
-    used: str = DefaultColors.used
-    last_used: str = DefaultColors.last_used
+    alias: str = DefaultColors.red
+    template: str = DefaultColors.green
+    description: str = DefaultColors.orange
+    date_created: str = DefaultColors.blue
+    last_updated: str = DefaultColors.light_blue
+    used: str = DefaultColors.purple
+    last_used: str = DefaultColors.light_purple
 
 
 @dataclass(frozen=True)
 class UiVariableColors:
-    name: str = DefaultColors.name
-    value: str = DefaultColors.template
-    date_created: str = DefaultColors.date_created
-    last_updated: str = DefaultColors.updated
+    name: str = DefaultColors.red
+    value: str = DefaultColors.green
+    date_created: str = DefaultColors.blue
+    last_updated: str = DefaultColors.light_blue
 
 
 @dataclass(frozen=True)
 class UiTagColors:
-    name: str = DefaultColors.name
-    description: str = DefaultColors.description
-    date_created: str = DefaultColors.date_created
-    last_updated: str = DefaultColors.updated
+    name: str = DefaultColors.red
+    description: str = DefaultColors.orange
+    date_created: str = DefaultColors.blue
+    last_updated: str = DefaultColors.light_blue
 
 
 @dataclass(frozen=True)
