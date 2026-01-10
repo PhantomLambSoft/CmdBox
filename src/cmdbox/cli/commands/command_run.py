8ea1f8eac7c2aa45c95a4d25c049b015d9166f23
Kyle <kyle.hickey222@gmail.com>
2026-01-10 15:01:18 -0600

    Refactor `run` command to support additional options, separate `preview`, and use run_handler

--- src/cmdbox/cli/commands/command_run.py
+++ src/cmdbox/cli/commands/command_run.py
@@ -5,6 +5,12 @@ import typer
 from cmdbox import container
 from cmdbox.cli.commands.command_crud import cli_guard
 from cmdbox.cli.completions.commands import complete_command_aliases
+from cmdbox.cli.handlers.run_handler import (
+    run_preview_command,
+    RawRunContext,
+    run_run_command,
+)
+
 
 app = typer.Typer(no_args_is_help=True)
 
@@ -19,22 +25,83 @@ def run(
             autocompletion=complete_command_aliases,
         ),
     ],
-    preview: Annotated[
+    preview_cmd: Annotated[bool, typer.Option("--preview", "-p", is_flag=True)] = False,
+    cwd: Annotated[
+        str,
+        typer.Option(
+            "--cwd", "-d", help="The working directory for the command execution."
+        ),
+    ] = None,
+    env: Annotated[
+        list[str] | str,
+        typer.Option(
+            "--env", "-e", help="The environment variables to set for the command."
+        ),
+    ] = None,
+    capture: Annotated[
         bool,
         typer.Option(
-            "--preview",
-            "-p",
-            is_flag=True,
-            help="Preview the command execution, but do not execute.",
+            "--capture", "-c", is_flag=True, help="Capture the command output."
         ),
     ] = False,
+    shell: Annotated[
+        str,
+        typer.Option(
+            "--shell", "-s", help="The shell to use for the command execution."
+        ),
+    ] = None,
 ):
-    console = container.get_console()
-    run_service = container.get_run_service()
-    if preview:
-        result = run_service.preview(alias)
-        console.print_run_preview(result)
-    else:
-        ex_result = run_service.run(alias)
-        if ex_result.stderr:
-            console.error(ex_result.stderr)
+    if preview_cmd:
+        preview(alias=alias, cwd=cwd, env=env, capture=capture, shell=shell)
+        return
+    ctx = RawRunContext(cwd=cwd, env=env, capture=capture, shell=shell)
+    run_run_command(
+        alias=alias,
+        run_ctx=ctx,
+        get_run_service=container.get_run_service,
+        get_console=container.get_console,
+    )
+
+
+@app.command("preview")
+@cli_guard
+def preview(
+    alias: Annotated[
+        str,
+        typer.Argument(
+            help="The alias of the command to run.",
+            autocompletion=complete_command_aliases,
+        ),
+    ],
+    cwd: Annotated[
+        str,
+        typer.Option(
+            "--cwd", "-d", help="The working directory for the command execution."
+        ),
+    ] = None,
+    env: Annotated[
+        list[str] | str,
+        typer.Option(
+            "--env", "-e", help="The environment variables to set for the command."
+        ),
+    ] = None,
+    capture: Annotated[
+        bool,
+        typer.Option(
+            "--capture", "-c", is_flag=True, help="Capture the command output."
+        ),
+    ] = False,
+    shell: Annotated[
+        str,
+        typer.Option(
+            "--shell", "-s", help="The shell to use for the command execution."
+        ),
+    ] = None,
+):
+    ctx = RawRunContext(cwd=cwd, env=env, capture=capture, shell=shell)
+    run_preview_command(
+        alias=alias,
+        run_ctx=ctx,
+        get_run_service=container.get_run_service,
+        get_console=container.get_console,
+    )
