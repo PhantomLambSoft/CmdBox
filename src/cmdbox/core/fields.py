2cc63065a890e0fbcc534315a3ded6395afb2f4a
Kyle <kyle.hickey222@gmail.com>
2026-02-14 11:37:09 -0600

    Define allowed display and search field groups for commands, variables, and tags

---
+++ src/cmdbox/core/fields.py
@@ -0,0 +1,44 @@
+COMMAND_DISPLAY_FIELDS = [
+    "alias",
+    "template",
+    "description",
+    "date_created",
+    "last_updated",
+    "used",
+    "last_used",
+]
+COMMAND_SEARCH_FIELDS = [
+    "alias",
+    "template",
+    "description",
+    "date_created",
+    "last_updated",
+    "used",
+    "last_used",
+]
+
+VARIABLE_DISPLAY_FIELDS = [
+    "name",
+    "value",
+    "date_created",
+    "last_updated",
+]
+VARIABLE_SEARCH_FIELDS = [
+    "name",
+    "value",
+    "date_created",
+    "last_updated",
+]
+
+TAGS_DISPLAY_FIELDS = [
+    "name",
+    "description",
+    "date_created",
+    "last_updated",
+]
+TAGS_SEARCH_FIELDS = [
+    "name",
+    "description",
+    "date_created",
+    "last_updated",
+]
