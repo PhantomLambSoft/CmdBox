fb2b65b8a06c98cfdb2767e63dd391ae4a2f9187
Kyle <kyle.hickey222@gmail.com>
2026-02-22 11:53:14 -0600

    Set default values in prompt functions to empty strings instead of None
    This was causing errors with prompt-toolkit.

--- src/cmdbox/cli/prompts/prompts.py
+++ src/cmdbox/cli/prompts/prompts.py
@@ -9,37 +9,37 @@ from cmdbox.cli.prompts.validators import (
 )
 
 
-def prompt_for_alias(validator: AliasValidator, default: str = None) -> str:
+def prompt_for_alias(validator: AliasValidator, default: str = "") -> str:
     alias = prompt("Enter alias: ", validator=validator, default=default)
     return alias
 
 
-def prompt_for_template(validator: TemplateValidator, default: str = None) -> str:
+def prompt_for_template(validator: TemplateValidator, default: str = "") -> str:
     template = prompt(
         "Enter template: ", multiline=True, validator=validator, default=default
     )
     return template
 
 
-def prompt_for_description(default: str = None) -> str:
+def prompt_for_description(default: str = "") -> str:
     description = prompt("Enter description: ", default=default)
     return description
 
 
 def prompt_for_name(
-    validator: NameValidator | TagNameValidator, default: str = None
+    validator: NameValidator | TagNameValidator, default: str = ""
 ) -> str:
     name = prompt("Enter name: ", validator=validator, default=default)
     return name
 
 
-def prompt_for_value(default: str = None) -> str:
+def prompt_for_value(default: str = "") -> str:
     value = prompt("Enter variable value: ", default=default)
     return value
 
 
 def prompt_for_tags(
-    tag_completer: TagCompleter, validator: TagNameValidator, default: str = None
+    tag_completer: TagCompleter, validator: TagNameValidator, default: str = ""
 ) -> list[str] | None:
     tags = prompt(
         "Enter tags (comma-separated): ",
