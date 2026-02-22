f660934326623a6368dab8650acd220513062bfb
Kyle <kyle.hickey222@gmail.com>
2026-02-21 18:04:52 -0600

    Remove redundant tag display logic from `variable_presenter`

--- src/cmdbox/cli/ui/presenters/variable_presenter.py
+++ src/cmdbox/cli/ui/presenters/variable_presenter.py
@@ -53,12 +53,6 @@ def render_variable(variable: Variable):
         header, _, extractor = value
         rows.append((header, extractor(variable)))
     var_display = kv_table(rows)
-
-    tags = [link.tag.name for link in variable.tags]
-    if len(tags) > 0:
-        tag_display = tag_block(tags=tags)
-        return stack(var_display, tag_display)
-
     return var_display
 
 
