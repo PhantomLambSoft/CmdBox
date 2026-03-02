ee9ae03df30055473e2576af14b8e5f52d5b50e8
Kyle <kyle.hickey222@gmail.com>
2026-03-02 17:52:42 -0600

    Add debug logging to `init` and `detect_shell` functions in init.py

--- src/cmdbox/cli/commands/init.py
+++ src/cmdbox/cli/commands/init.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Annotated
 
 import typer
@@ -9,6 +10,8 @@ from cmdbox.cli.handlers.init_handler import run_detect_shell, run_init_command
 app = typer.Typer(no_args_is_help=False)
 
 cli_guard = make_cli_guard(container.get_console)
+
+log = logging.getLogger(__name__)
 
 
 @app.command()
@@ -37,6 +40,7 @@ def init(
     """
     Initialize CmdBox for a shell environment.
     """
+    log.debug("init.init called. shell=%s, install=%s", shell, install)
     run_init_command(
         shell=shell, install=install, path=path, get_console=container.get_console
     )
@@ -46,4 +50,5 @@ def init(
 @cli_guard
 def detect_shell() -> None:
     """Detects and prints the current shell being used."""
+    log.debug("init.shell called.")
     run_detect_shell(get_console=container.get_console)
