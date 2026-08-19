f0a639b6dffae85a76ba6f2e21d075f8548dfa1b
MalloyDelacroix <noreply@phantomlamb.com>
2026-08-19 18:28:29 -0500

    Add `Command` model

---
+++ internal/models/command.go
@@ -0,0 +1,25 @@
+package models
+
+import "time"
+
+// Command represents a saved command - called by its alias, the template is what gets executed. Other optional
+// execution context is stored and used when the command is executed.
+type Command struct {
+	ID          uint   `gorm:"primaryKey"`
+	Name        string `gorm:"uniqueIndex:idx_command_alias_profile;not null"`
+	Template    string `gorm:"not null"`
+	Description string `gorm:"size:255"`
+
+	Cwd     *string
+	Shell   *string
+	Env     *string
+	Timeout *int
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Used      int
+	LastUsed  *time.Time
+
+	ProfileID uint    `gorm:"uniqueIndex:idx_command_alias_profile;not null"`
+	Profile   Profile `gorm:"foreignKey:ProfileID;constraint:OnDelete:CASCADE"`
+}
